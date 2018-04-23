3c44f038ca6e8e2b95d412ccd1b13fe41412c8cb
Yuri Nikulin <kowkogab@gmail.com>
2018-04-23 17:51:10 +0400

    test

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,16 +1,60 @@
 import React from 'react';
-import List from "./List";
-import Form from "./Form";
+import { connect } from 'react-redux';
 
-const App = () => {
-    return(
-        <div>
-            <h2>Articless</h2>
-            <List />
-            <h2>Add a new article</h2>
-            <Form />
-        </div>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            song: ''
+        }
+    }
+    addTrack = (event) => {
+        event.preventDefault();
+        this.props.onAddTrack(this.state.song);
+        console.log(this.state.song);
+        this.setState({
+            song: ''
+        })
+    }
+
+    componentDidMount() {
+        console.log(this.props);
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+
+    render() {
+        return(
+            <div>
+                <form onSubmit={this.addTrack}>
+                    <input id="song" onChange={this.handleChange} value={this.state.song} type="text" />
+                    <button type="submit">Add track</button>
+                </form>
+                <ul>
+                    {this.props.testStore.map((track, index) => {
+                        return (
+                            <li key={index}>
+                                {track}
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        );  
+    } 
 }
 
-export default App;
+export default connect(
+        state => ({
+            testStore: state
+        }),
+        dispatch => ({
+            onAddTrack: (trackName) => {
+                dispatch({type: 'ADD_TRACK', payload: trackName});
+            }
+        })
+    )(App);
--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import store from "./store/index";
+import { createStore } from "redux";
+
 import App from "./components/App.js";
-render(
+const initialState = [
+    'SLTS',
+    'ES'
+];
+
+const playlist = (state = initialState, action) => {
+    switch(action.type) {
+        case 'ADD_TRACK':
+            return [...state, action.payload];
+        default:
+            return state;    
+    }
+    return state;
+}
+
+const store = createStore(playlist);
+
+ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>,
